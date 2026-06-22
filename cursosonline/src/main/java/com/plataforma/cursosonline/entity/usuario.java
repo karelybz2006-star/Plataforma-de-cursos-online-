e04156f4c6675c4f043e30ee9f7405f5e5851d67
Karely Baque <karelybz2006@gmail.com>
2026-06-22 14:13:26 -0500

    Crea entidad Usuario

---
+++ cursosonline/src/main/java/com/plataforma/cursosonline/entity/usuario.java
@@ -0,0 +1,31 @@
+package com.plataforma.cursosonline.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
+}
